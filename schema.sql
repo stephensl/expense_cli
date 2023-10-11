54b8fe6012e09a4aa1ecb955fb1298c43de55be0
Lawton Stephens <stephens.lawton@gmail.com>
2023-10-11 19:44:21 -0400

    interacting with database in code

--- schema.sql
+++ schema.sql
@@ -2,5 +2,17 @@ CREATE TABLE expenses (
   id serial PRIMARY KEY, 
   amount numeric(8, 2) NOT NULL, 
   memo varchar(50) NOT NULL, 
-  created_on timestamp DEFAULT CURRENT_TIMESTAMP
+  created_on date NOT NULL DEFAULT CURRENT_DATE
 );
+
+ALTER TABLE expenses
+  ADD CHECK (amount > 0.00); 
+
+--- sample data
+-- INSERT INTO expenses (amount, memo)
+--   VALUES (3.75, 'Coffee'), 
+--          (11.00, 'Lunch'), 
+--          (32.90, 'Gas for car'), 
+--          (62.00, 'tickets');
+
+  
